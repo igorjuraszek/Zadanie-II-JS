01097a7bfa9305e1a6d769d1d29e59ed263ce538
Dawid Damek <dawid.damek98@gmail.com>
2022-04-22 19:49:42 +0200

    add select username filter

--- js/main.js
+++ js/main.js
@@ -1,125 +1,153 @@
-let searchInput = ''
-let showMyPosts = false
-const form = document.getElementById('data')
+let searchInput = '';
+let showMyPosts = false;
+let selectOption = 'allUsers';
+const select = document.querySelector('#filterByUsername');
+const usersFromLocalStorage = JSON.parse(
+  window.localStorage.getItem('usersDatabase')
+);
+const form = document.getElementById('data');
 window.onload = function () {
-	if (!window.localStorage.getItem('currentUser')) {
-		window.location.href = 'start.html'
-	}
-}
+  if (!window.localStorage.getItem('currentUser')) {
+    window.location.href = 'start.html';
+  }
+};
 
 function logout() {
-	window.localStorage.removeItem('currentUser')
-	window.location.href = 'start.html'
+  window.localStorage.removeItem('currentUser');
+  window.location.href = 'start.html';
 }
 
 const allPosts = [
-	{
-		id: 1,
-		ownerId: 1,
-		title: 'Tytuł testowy 1',
-		body: 'Zawartość testowa 1',
-		likesCount: 0,
-		isDeleted: false,
-	},
-	{
-		id: 2,
-		ownerId: 1,
-		title: 'Tytuł testowy 2',
-		body: 'Zawartość testowa 2',
-		likesCount: 0,
-		isDeleted: false,
-	},
-	{
-		id: 3,
-		ownerId: 1,
-		title: 'Tytuł testowy 3',
-		body: 'Zawartość testowa 3',
-		likesCount: 0,
-		isDeleted: false,
-	},
-]
+  {
+    id: 1,
+    ownerId: 1,
+    title: 'Tytuł testowy 1',
+    body: 'Zawartość testowa 1',
+    likesCount: 0,
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    ownerId: 1,
+    title: 'Tytuł testowy 2',
+    body: 'Zawartość testowa 2',
+    likesCount: 0,
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    ownerId: 1,
+    title: 'Tytuł testowy 3',
+    body: 'Zawartość testowa 3',
+    likesCount: 0,
+    isDeleted: false,
+  },
+];
 
-const findUserById = id => {
-	const usersFromLocalStorage = JSON.parse(window.localStorage.getItem('usersDatabase'))
+// const userToSelect = () => {
+// 	return
+// }
 
-	return usersFromLocalStorage.find(user => user.id === id)
-}
+const selectOptions = () => {
+  for (let i = 0; i < usersFromLocalStorage.length; i++) {
+    const userOption = document.createElement('option');
+    userOption.value = usersFromLocalStorage[i].username;
+    userOption.innerHTML = usersFromLocalStorage[i].username;
+    select.appendChild(userOption);
+  }
+};
+selectOptions();
+
+const updateSelection = (event) => {
+  selectOption = event.target.value;
+  show();
+};
+
+document
+  .querySelector('#filterByUsername')
+  .addEventListener('change', updateSelection);
+
+const findUserById = (id) => {
+  return usersFromLocalStorage.find((user) => user.id === id);
+};
 
 async function fetchPosts() {
-	const url = 'https://jsonplaceholder.typicode.com/posts?userId=1'
-	const response = await fetch(url)
-	const arrayOfObjects = await response.json()
-	arrayOfObjects.forEach(importedPost => {
-		postToImport = {
-			id: allPosts.length + 1,
-			ownerId: 1,
-			title: importedPost.title,
-			body: importedPost.body,
-			likesCount: 0,
-			isDeleted: false,
-		}
-		allPosts.push(postToImport)
-		show()
-	})
+  const url = 'https://jsonplaceholder.typicode.com/posts?userId=1';
+  const response = await fetch(url);
+  const arrayOfObjects = await response.json();
+  arrayOfObjects.forEach((importedPost) => {
+    postToImport = {
+      id: allPosts.length + 1,
+      ownerId: 1,
+      title: importedPost.title,
+      body: importedPost.body,
+      likesCount: 0,
+      isDeleted: false,
+    };
+    allPosts.push(postToImport);
+    show();
+  });
 }
 
-fetchPosts()
+fetchPosts();
 
-const addPost = event => {
-	event.preventDefault()
+const addPost = (event) => {
+  event.preventDefault();
 
-	const inputTitle = document.getElementById('input-title')
-	const inputBody = document.getElementById('input-body')
+  const inputTitle = document.getElementById('input-title');
+  const inputBody = document.getElementById('input-body');
 
-	const post = {
-		ownerId: loggedAs.id,
-		id: allPosts.length + 1,
-		title: inputTitle.value,
-		body: inputBody.value,
-		likesCount: 0,
-		isDeleted: false,
-	}
+  const post = {
+    ownerId: loggedAs.id,
+    id: allPosts.length + 1,
+    title: inputTitle.value,
+    body: inputBody.value,
+    likesCount: 0,
+    isDeleted: false,
+  };
 
-	if (post.title !== '' && post.body !== '') {
-		allPosts.push(post)
-	}
-	show()
-}
+  if (post.title !== '' && post.body !== '') {
+    allPosts.push(post);
+  }
+  show();
+};
 
-const searchPost = event => {
-	searchInput = event.target.value
+const searchPost = (event) => {
+  searchInput = event.target.value;
 
-	console.log(searchInput)
-	show()
-}
+  console.log(searchInput);
+  show();
+};
 
-const changeShowMyPosts = event => {
-	showMyPosts = event.target.checked
-	show()
-}
+const changeShowMyPosts = (event) => {
+  showMyPosts = event.target.checked;
+  show();
+};
 
-document.querySelector('#search-post').addEventListener('keyup', searchPost)
-document.querySelector('#my-post-checkbox').addEventListener('change', changeShowMyPosts)
+document.querySelector('#search-post').addEventListener('keyup', searchPost);
+document
+  .querySelector('#my-post-checkbox')
+  .addEventListener('change', changeShowMyPosts);
 
-form.addEventListener('submit', addPost)
+form.addEventListener('submit', addPost);
 
 const showButtons = (post, canUserLikePost) => {
-	if (canUserLikePost) {
-		return `
+  if (canUserLikePost) {
+    return `
       <button id="like-${post.id}" class="btn btn-success">  +5 </button>
       <button id="dislike-${post.id}" class="btn btn-danger"> -10 </button>
-    `
-	} else {
-		return `
+    `;
+  } else {
+    return `
      <button id="delete-${post.id}" class="btn btn-dark"> delete </button>
-     `
-	}
-}
+     `;
+  }
+};
 
 const createPost = (post, canUserLikePost) => {
-	return `
+  return `
     <div id=${post.id}>
-			 <p>${findUserById(post.ownerId).username}</p>
+			<p>${findUserById(post.ownerId).username}</p>
       <p>${post.title}.</p>
       <p>${post.body}.</p>
       <p>Liczba lajków: ${post.likesCount}</p>
@@ -127,59 +155,71 @@ const createPost = (post, canUserLikePost) => {
          
 ${showButtons(post, canUserLikePost)}
     </p>
-    </div>`
-}
+    </div>`;
+};
 
 function show() {
-	const postToShow = allPosts
-		.filter(post => {
-			const isThisMyPost = loggedAs.id === post.ownerId
-			if (showMyPosts) {
-				return isThisMyPost && post.title.includes(searchInput)
-			}
+  const postToShow = allPosts
+    .filter((post) => {
+      if (selectOption === 'allUsers') {
+        return true;
+      }
+      if (selectOption === findUserById(post.ownerId).username) {
+        return true;
+      }
+    })
+    .filter((post) => {
+      const isThisMyPost = loggedAs.id === post.ownerId;
+      if (showMyPosts && selectOption === 'allUsers') {
+        return isThisMyPost && post.title.includes(searchInput);
+      }
 
-			return post.title.includes(searchInput)
-		})
-		.filter(function (post) {
-			return post.isDeleted == false
-		})
-		.reverse()
-		.map(post => {
-			const canUserLikePost = loggedAs.id !== post.ownerId
+      return post.title.includes(searchInput);
+    })
+    .filter(function (post) {
+      return post.isDeleted == false;
+    })
+    .reverse()
+    .map((post) => {
+      const canUserLikePost = loggedAs.id !== post.ownerId;
 
-			return createPost(post, canUserLikePost)
-		})
-	const html = postToShow.join('')
+      return createPost(post, canUserLikePost);
+    });
+  const html = postToShow.join('');
 
-	document.querySelector('#postsContainer').innerHTML = html
-	const postCount = allPosts.filter(function (post) {
-		return post.isDeleted == false
-	}).length
-	document.querySelector('#numberPost').textContent = `Liczba postów: ${postToShow.length}/${postCount}`
+  document.querySelector('#postsContainer').innerHTML = html;
+  const postCount = allPosts.filter(function (post) {
+    return post.isDeleted == false;
+  }).length;
+  document.querySelector(
+    '#numberPost'
+  ).textContent = `Liczba postów: ${postToShow.length}/${postCount}`;
 }
 
-document.querySelector('#postsContainer').addEventListener('click', event => {
-	console.log(event)
-	const id = event.target.id
-	const btnType = id.split('-')[0]
-	const postId = parseInt(id.split('-')[1])
-	console.log(btnType, postId, id)
-	if (btnType === 'like') {
-		allPosts[postId - 1].likesCount = allPosts[postId - 1].likesCount + 5
-		show()
-	}
-	if (btnType === 'dislike') {
-		allPosts[postId - 1].likesCount = allPosts[postId - 1].likesCount - 10
-		show()
-	}
-	if (btnType === 'delete') {
-		allPosts[postId - 1].isDeleted = true
-		show()
-	}
-})
+document.querySelector('#postsContainer').addEventListener('click', (event) => {
+  console.log(event);
+  const id = event.target.id;
+  const btnType = id.split('-')[0];
+  const postId = parseInt(id.split('-')[1]);
+  console.log(btnType, postId, id);
+  if (btnType === 'like') {
+    allPosts[postId - 1].likesCount = allPosts[postId - 1].likesCount + 5;
+    show();
+  }
+  if (btnType === 'dislike') {
+    allPosts[postId - 1].likesCount = allPosts[postId - 1].likesCount - 10;
+    show();
+  }
+  if (btnType === 'delete') {
+    allPosts[postId - 1].isDeleted = true;
+    show();
+  }
+});
 
-const userFromLocalStorage = window.localStorage.getItem('currentUser')
-const loggedAs = JSON.parse(userFromLocalStorage)
-const displayedUsername = loggedAs.username
+const userFromLocalStorage = window.localStorage.getItem('currentUser');
+const loggedAs = JSON.parse(userFromLocalStorage);
+const displayedUsername = loggedAs.username;
 
-document.querySelector('#logged-as').textContent = `Zalogowano jako: ${displayedUsername}`
+document.querySelector(
+  '#logged-as'
+).textContent = `Zalogowano jako: ${displayedUsername}`;
